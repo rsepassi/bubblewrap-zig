6e16f9b4b074a8e2dbfed232b5c228d471fbfafd
ryan <rsepassi@gmail.com>
2023-10-24 17:43:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# bubblewrap build.zig
+
+https://github.com/containers/bubblewrap/releases
+
+[v0.8.0](https://github.com/containers/bubblewrap/releases/download/v0.8.0/bubblewrap-0.8.0.tar.xz) (2023-02-27)
+
+`config.h` generated with:
+
+```
+# in alpine:3.18.4
+# apk add gcc musl-dev libcap-dev
+./configure \
+  --disable-selinux \
+  --enable-require-userns=yes \
+```
---
+++ build.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const libcap = b.dependency("libcap_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const libcap_lib = libcap.artifact("cap");
+
+    const exe = b.addExecutable(.{
+        .name = "bwrap",
+        .target = target,
+        .optimize = optimize,
+        .linkage = .static,
+    });
+    exe.addIncludePath(.{.path = "include"});
+    exe.addCSourceFiles(&c_sources, &[_][]const u8{});
+    exe.linkLibrary(libcap_lib);
+    exe.linkLibC();
+    exe.strip = true;
+    b.installArtifact(exe);
+}
+
+const c_sources = [_][]const u8 {
+    "src/bind-mount.c",
+    "src/bubblewrap.c",
+    "src/network.c",
+    "src/utils.c",
+};
